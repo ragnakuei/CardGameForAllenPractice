c1c3b1dd03ff1a7b381ab72c4e52f3f8784faf45
Kuei <ragnakuei@livemail.tw>
2021-05-18 12:15:36 +0800

    Add project files.

---
+++ CardGameForAllenPractice/牌組.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardGameForAllenPractice
+{
+    public class 牌組
+    {
+        public 牌組(params 牌組判斷[] 牌組判斷s)
+        {
+
+        }
+    }
+}
---
+++ CardGameForAllenPractice/牌組判斷.cs
@@ -0,0 +1,11 @@
+﻿namespace CardGameForAllenPractice
+{
+    public interface 牌組判斷
+    {
+        牌組結果 判斷();
+    }
+
+    public enum 牌組結果
+    { 
+    }
+}

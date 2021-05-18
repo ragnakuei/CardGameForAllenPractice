5c56f73c32a4cb02d0eb6b97b8af085a6a28d88a
Kuei <ragnakuei@livemail.tw>
2021-05-18 12:23:53 +0800

    因工作中斷

---
+++ CardGameForAllenPractice/Cards.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CardGameForAllenPractice
+{
+    public class Cards
+    {
+        private readonly CardsValidator[] _cardsValidators;
+
+        public Cards(params CardsValidator[] cardsValidators)
+        {
+            _cardsValidators = cardsValidators;
+        }
+
+        public CardsResult Get(List<Card> card)
+        {
+            foreach (var cardsValidator in _cardsValidators)
+            {
+                if (cardsValidator.TryGetResult(card, out var result))
+                {
+                    return result;
+                }
+            }
+
+            return CardsResult.NoResult;
+        }
+    }
+
+    public class Card
+    {
+    }
+}
---
+++ CardGameForAllenPractice/CardsValidator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CardGameForAllenPractice
+{
+    public interface CardsValidator
+    {
+        bool TryGetResult(List<Card> card, out CardsResult result);
+    }
+
+    public enum CardsResult
+    {
+        NoResult
+    }
+}
